3f36fcf041bfcd7e9485efcb7a8a835deab87a33
Visualised <Visually@protonmail.com>
2017-12-17 22:32:30 +0100

    Day 1
    Part 1 + Part 2

---
+++ AdventOfCode_Day1/CircularIntArray.cs
@@ -0,0 +1,65 @@
+﻿// Yeah, i can do this assignment in one function i know.
+// I probably make a second version without creating my own
+// datatype and all of this mess. But it works ¯\_(ツ)_/¯
+
+namespace AdventOfCode_Day1
+{
+    class CircularIntArray
+    {
+        private readonly int[] _array;
+        private int _index = 0;
+
+        public CircularIntArray(int[] input)
+        {
+            _array = input;
+        }
+
+        public int Current()
+        {
+            return _array[_index];
+        }
+
+        public int GetValueOfIndex(int input)
+        {
+            return _array[input];
+        }
+
+        public int MoveNext()
+        {
+            if (++_index >= _array.Length)
+            {
+                _index = 0;
+            }
+
+            return _array[_index];
+        }
+
+        public int SkipAndMoveNext(int skipAmount)
+        {
+            int tempIndex;
+            if (_index + skipAmount >= _array.Length)
+            {
+                tempIndex = skipAmount - TillEnd();
+                _index++;
+            }  
+            else
+            {
+                tempIndex = _index + skipAmount;
+                _index++;
+            }
+
+            return _array[tempIndex];
+        }
+
+        private int TillEnd()
+        {
+            int counter = 0;
+            for (int i = _index; i < _array.Length; i++)
+            {
+                counter++;
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ AdventOfCode_Day1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Yeah, i can do this assignment in one function i know.
+// I probably make a second version without creating my own
+// datatype and all of this mess. But it works ¯\_(ツ)_/¯
+
+using System;
+using System.IO;
+
+namespace AdventOfCode_Day1
+{
+    class Program
+    {
+
+        private static int SumAllNextMatchingDigits(int[] input)
+        {
+            CircularIntArray unsolvedCaptchaArray = new CircularIntArray(input);
+            int solvedCaptcha = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (unsolvedCaptchaArray.Current() == unsolvedCaptchaArray.MoveNext())
+                {
+                    solvedCaptcha += unsolvedCaptchaArray.Current();
+                }
+            }
+
+            return solvedCaptcha;       
+        }
+
+        private static int SkipAndSumMatchingDigits(int[] input, int skipAmount)
+        {
+            CircularIntArray unsolvedCaptchaArray = new CircularIntArray(input);
+            int solvedCaptcha = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (unsolvedCaptchaArray.Current() == unsolvedCaptchaArray.SkipAndMoveNext(skipAmount))
+                {
+                    solvedCaptcha += unsolvedCaptchaArray.GetValueOfIndex(i); // I can't use .Current() because we are on next digit already :<
+                }
+            }
+
+            return solvedCaptcha;
+        }
+
+        static void Main()
+        {
+            string unsolvedCaptcha = File.ReadAllText("input.txt");
+
+            int arrayLength = unsolvedCaptcha.Length;
+            int[] unsolvedCaptchaArray = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                unsolvedCaptchaArray[i] = Convert.ToInt32(Char.GetNumericValue(unsolvedCaptcha[i]));
+            }
+
+            Console.WriteLine("Part 1: {0}", SumAllNextMatchingDigits(unsolvedCaptchaArray));
+
+            int skipAmount = arrayLength / 2;
+            Console.WriteLine("Part 2: {0}", SkipAndSumMatchingDigits(unsolvedCaptchaArray, skipAmount));
+
+            Console.ReadKey();
+        }
+    }
+}
